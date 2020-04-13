86cbf67c4a9b93127665fc573a4cd0d8facab82b
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-04-13 13:13:49 +0700

    ADD | Inisialiasi  class, variable, dan method main

---
+++ Tubes_Kriptografi Hill Chiper.py
@@ -0,0 +1,40 @@
+'''
+    +----------------------------------------+
+	|        MUHAMMAD REZKI ANANDA           |
+    +----------------------------------------+
+	|        SI/43/09 (1202190044)	 	     |
+    +----------------------------------------+
+'''
+
+class HillChiper():
+    def getKey(self):
+        return None
+
+    def encryptData(self):
+        return None
+
+    def decryptData(self):
+        return None
+
+    def processEncrypt(self):
+        return None
+    
+    def processDecrypt(self):
+        return None
+
+class OutputHillChiper(HillChiper):
+    def showMenu(self):
+        return None
+
+if __name__ == "__main__":
+
+    isMessage = ""
+    isKey = ""
+
+    keyMatrix = []
+    messageMatrix = []
+    cipherMatrix = []
+
+    ftr = OutputHillChiper()
+    ftr.showMenu()
+
