d67b24756221aa47e7cec260c2dacf2846e88837
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-04-18 09:11:56 +0700

    ADD | Record Table #2

--- Support File/tubes_kriptografi.sql
+++ Support File/tubes_kriptografi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2020 at 03:34 AM
+-- Generation Time: Apr 18, 2020 at 04:11 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -38,8 +38,7 @@ CREATE TABLE `dekripsi` (
 --
 
 INSERT INTO `dekripsi` (`id_dekripsi`, `kata_awal_de`, `hasil_dekripsi`, `tanggal`) VALUES
-(1, 'POH', 'ACT', '2020-04-18 08:24:13'),
-(2, 'POH', 'ACT', '2020-04-18 08:31:21');
+(1, 'POH', 'ACT', '2020-04-18 09:09:29');
 
 -- --------------------------------------------------------
 
@@ -59,13 +58,7 @@ CREATE TABLE `enkripsi` (
 --
 
 INSERT INTO `enkripsi` (`id_enkripsi`, `kata_awal_en`, `hasil_enkripsi`, `tanggal`) VALUES
-(1, 'ACT', 'ACT', '0000-00-00 00:00:00'),
-(2, 'ACT', 'ACT', '2020-04-18 08:17:20'),
-(3, 'ACT', 'ACT', '2020-04-18 08:17:55'),
-(4, 'ACT', 'ACT', '2020-04-18 08:18:15'),
-(5, 'ACT', 'POH', '2020-04-18 08:20:01'),
-(6, 'DAD', 'VRB', '2020-04-18 08:24:41'),
-(7, 'ACT', 'POH', '2020-04-18 08:31:18');
+(1, 'ACT', 'POH', '2020-04-18 09:09:23');
 
 --
 -- Indexes for dumped tables
@@ -91,12 +84,12 @@ ALTER TABLE `enkripsi`
 -- AUTO_INCREMENT for table `dekripsi`
 --
 ALTER TABLE `dekripsi`
-  MODIFY `id_dekripsi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_dekripsi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `enkripsi`
 --
 ALTER TABLE `enkripsi`
-  MODIFY `id_enkripsi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_enkripsi` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
